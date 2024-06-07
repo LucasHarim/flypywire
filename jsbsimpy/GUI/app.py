467171724d682df959c8bb0beb206ff90d695121
LucasHarim <harimlgc@usp.br>
2024-06-07 16:30:21 -0300

    General gui_components improvements. Specifying IC accordingly to the aircraft name

--- jsbsimpy/GUI/app.py
+++ jsbsimpy/GUI/app.py
@@ -3,37 +3,56 @@ from typing import Tuple
 
 from jsbsimpy.GUI.gui_components import (
 	NavigationFrame,
-	InitialConditionFrame)
+	InitialConditionFrame,
+	AircraftFrame,
+	SocketConfigFrame)
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 class App(customtkinter.CTk):
 
-	def __init__(self, title: str, dimensions: Tuple[int, int]):
+	def __init__(self, title: str, dimensions: Tuple[int, int], jsbsim_root: str):
+
+		self.jsbsim_root = jsbsim_root
 
 		super().__init__()
         
 		self.title(title)
 		self.geometry = f'{dimensions[0]}x{dimensions[1]}'
-        
+		self.resizable(width=False, height=False)
+		
 		# configure grid layout (4x4)
-		self.grid_columnconfigure(1, weight=1)
-		self.grid_columnconfigure((2, 3), weight=0)
-		self.grid_rowconfigure((0, 1, 2), weight=1)
+		# self.grid_columnconfigure(1, weight=1)
+		# self.grid_columnconfigure((2, 3), weight=0)
+		# self.grid_rowconfigure((0, 1, 2), weight=1)
+		
+		_frame_rows = iter(range(100))
+		_frame_columns = iter(range(100))
 
+		
 		self.nav_frame = NavigationFrame(self)
-		self.nav_frame.grid(row=0, column=0, padx = 10, pady = 10, sticky="nsew")
+		self.nav_frame.grid(row=next(_frame_rows), column=next(_frame_columns), padx = 10, pady = 10, sticky="nsew")
+		
+		self.aicraft_frame = AircraftFrame(self, jsbsim_root = self.jsbsim_root)
+		self.aicraft_frame.grid(row = 0, column = next(_frame_columns), padx = 10, pady = (10, 0))
 
 		self.ic_frame = InitialConditionFrame(self)
-		self.ic_frame.grid(row = 0, column = 1, padx = 10, pady = 10, sticky="ns")
+		self.ic_frame.grid(row = 0, column = next(_frame_columns), padx = 10, pady = 10,sticky="ns")
+		
+		self.socket_config_frame = SocketConfigFrame(self)
+		self.socket_config_frame.grid(row = 0, column = next(_frame_columns), padx = 10, pady = 10)
+		
 
 if __name__ == '__main__':
-
-    app = App(
-        title="My app",
-        dimensions = (1100, 550))
+	
+	_JSBSim_ROOT = 'C:\\Users\\harim\\AppData\\Local\\JSBSim'
     
-    app.mainloop()
+	app = App(
+		title="Just Flying",
+		dimensions = (1100, 550),
+		jsbsim_root=_JSBSim_ROOT)
+
+	app.mainloop()
 
 
--- jsbsimpy/GUI/gui_components.py
+++ jsbsimpy/GUI/gui_components.py
@@ -1,12 +1,45 @@
+import os
+from typing import List, Iterable, Union
 from customtkinter import (
     CTkFrame,
     CTkLabel,
     CTkButton,
     CTkFont,
-    CTkEntry)
+    CTkEntry,
+    CTkComboBox)
 
+
+class CTkEntryType(CTkEntry):
+
+    def __init__(self, master, valid_type: Union[float, int, str] = str, **kwargs):
     
+        super().__init__(master, **kwargs)
         
+        self.valid_type = valid_type
+        self.bind("<KeyRelease>", self.validate_type)
+    
+    def validate_type(self, event=None):
+        
+        entry_value = self.get()
+        if self.get():
+            
+            if self.valid_type == int:
+                
+                if not entry_value.isdigit():
+                    
+                    # If the entry content is not a digit, remove the last character
+                    self.delete(len(entry_value)-1, 'end')
+            
+            if self.valid_type == float:
+                
+                if entry_value != '-':
+                    try:
+                        float(entry_value)
+                    except ValueError:
+                        # If the entry content is not a valid float, remove the last character
+                        self.delete(len(entry_value)-1, 'end')
+            
+
 class NavigationFrame(CTkFrame):
 
     def __init__(self, master,**kwargs):
@@ -52,10 +85,10 @@ class InitialConditionFrame(CTkFrame):
             compound = 'center' , font = CTkFont(size = 12))
         self.loc_label.grid(row = 0, column = 0, padx = 10, pady = 10)
 
-        self.latitude_entry = CTkEntry(self.loc_frame, placeholder_text = 'latitude')
+        self.latitude_entry = CTkEntryType(self.loc_frame, valid_type=float ,placeholder_text = 'latitude')
         self.latitude_entry.grid(row = 1, column = 0, padx = 5, pady = 5)
 
-        self.longitude_entry = CTkEntry(self.loc_frame, placeholder_text = 'longitude')
+        self.longitude_entry = CTkEntryType(self.loc_frame, valid_type=float,placeholder_text = 'longitude')
         self.longitude_entry.grid(row = 2, column = 0, padx = 5, pady = 5)
 
         self.altitude_frame = CTkFrame(self)
@@ -65,7 +98,7 @@ class InitialConditionFrame(CTkFrame):
             compound = 'center' , font = CTkFont(size = 12))
         self.alt_label.grid(row = 0, column = 0, padx = 10, pady = 10)
         
-        self.altitude_entry = CTkEntry(self.altitude_frame, placeholder_text = 'altitude')
+        self.altitude_entry = CTkEntryType(self.altitude_frame, valid_type=float,placeholder_text = 'altitude')
         self.altitude_entry.grid(row = 1, column = 0, padx = 5, pady = 5)
 
 
@@ -78,13 +111,13 @@ class InitialConditionFrame(CTkFrame):
             compound = 'center' , font = CTkFont(size = 12))
         self.loc_label.grid(row = next(_vel_frame_rows), column = 0, padx = 10, pady = 10)
 
-        self.ubody_entry = CTkEntry(self.vel_frame, placeholder_text = 'ubody')
+        self.ubody_entry = CTkEntryType(self.vel_frame, valid_type=float,placeholder_text = 'ubody')
         self.ubody_entry.grid(row = next(_vel_frame_rows), column = 0, padx = 5, pady = 5)
 
-        self.vbody_entry = CTkEntry(self.vel_frame, placeholder_text = 'vbody')
+        self.vbody_entry = CTkEntryType(self.vel_frame, valid_type=float,placeholder_text = 'vbody')
         self.vbody_entry.grid(row = next(_vel_frame_rows), column = 0, padx = 5, pady = 5)
 
-        self.wbody_entry = CTkEntry(self.vel_frame, placeholder_text = 'wbody')
+        self.wbody_entry = CTkEntryType(self.vel_frame, valid_type=float,placeholder_text = 'wbody')
         self.wbody_entry.grid(row = next(_vel_frame_rows), column = 0, padx = 5, pady = 5)
 
         _attitude_frame_rows = iter(range(10))
@@ -97,19 +130,99 @@ class InitialConditionFrame(CTkFrame):
             compound = 'center' , font = CTkFont(size = 12))
         self.att_label.grid(row = next(_attitude_frame_rows), column = 0, padx = 10, pady = 10)
 
-        self.phi_entry = CTkEntry(self.attitude_frame, placeholder_text = 'phi')
+        self.phi_entry = CTkEntryType(self.attitude_frame, valid_type=float, placeholder_text = 'phi')
         self.phi_entry.grid(row = next(_attitude_frame_rows), column = 0, padx = 5, pady = 5)
 
-        self.theta_entry = CTkEntry(self.attitude_frame, placeholder_text = 'theta')
+        self.theta_entry = CTkEntryType(self.attitude_frame, valid_type=float,placeholder_text = 'theta')
         self.theta_entry.grid(row = next(_attitude_frame_rows), column = 0, padx = 5, pady = 5)
 
-        self.psi_entry = CTkEntry(self.attitude_frame, placeholder_text = 'psi')
+        self.psi_entry = CTkEntryType(self.attitude_frame, valid_type = float,placeholder_text = 'psi')
         self.psi_entry.grid(row = next(_attitude_frame_rows), column = 0, padx = 5, pady = 5)
+        
+    
+    def outputs(self) -> dict:
+        
+        _outputs = {
+            'Location': {
+                'latitude': self.latitude_entry.get(),
+                'longitude': self.longitude_entry.get()},
+        }
 
-    def _setup_frame(self) -> None:
 
-        ...
+class AircraftFrame(CTkFrame):
 
+    def __init__(self, master, jsbsim_root: str, **kwargs) -> None:
+
+        self.jsbsim_root = jsbsim_root
+        self.aircraft_path = os.path.join(self.jsbsim_root, 'aircraft')
+        self.available_aircrafts_in_root = self._get_aircrafts_in_jsbsim_root()
+
+        super().__init__(master, **kwargs)
+        
+        _frame_rows = iter(range(10))
+
+        self.frame_label = CTkLabel(self, text = 'Aircraft', compound='center', font = CTkFont(size = 12, weight='bold'))
+        self.frame_label.grid(row = next(_frame_rows), column = 0, padx = 10, pady = 10)
+        
+        self.aircraft_selection = CTkComboBox(self, values = [aircraft for aircraft in self.available_aircrafts_in_root])
+        self.aircraft_selection.grid(row = next(_frame_rows), column = 0, padx = 10, pady = 5)
+
+        # self.aircraft_selection_label = CTkLabel(self.aircraft_selection, text = '')
+        # self.aircraft_selection.grid(row = 0, column = 10, padx = 10, pady = 10)
+    
+    def _get_aircrafts_in_jsbsim_root(self) -> List[str]:
+        
+        try:
+    
+            # List all entries in the given path
+            entries = os.listdir(self.aircraft_path)
+            
+            # Filter out only directories
+            directories = [entry for entry in entries if os.path.isdir(os.path.join(self.aircraft_path, entry))]
+    
+            return directories
+
+        except FileNotFoundError:
+            return f"The path {self.aircraft_path} does not exist."
+
+        except PermissionError:
+            return f"Permission denied for accessing the path {self.aircraft_path}."
+    
+    
+    
+    def outputs(self) -> dict:
+        return {'aircraft_selected': self.aircraft_selection.get()}
+    
+
+class SocketConfigFrame(CTkFrame):
+
+    def __init__(self, master, **kwargs):
+
+        super().__init__(master, **kwargs)
+        
+        _frame_rows = iter(range(10))
+
+        self.frame_label = CTkLabel(self, text = 'Socket configuration', font = CTkFont(size = 12, weight='bold'))
+        self.frame_label.grid(row = next(_frame_rows), column = 0, padx = 5, pady = 5)
+        
+        
+        self.host_entry = CTkEntryType(self, placeholder_text='host')
+        self.host_entry.grid(row = next(_frame_rows), column = 0, padx = 5, pady = 5)
+        self.host_entry.bind("<KeyRelease>", self.display_address)
+
+        self.port_entry = CTkEntryType(self, valid_type=int,placeholder_text='port')
+        self.port_entry.grid(row = next(_frame_rows), column = 0, padx = 5, pady = 5)
+        self.port_entry.bind("<KeyRelease>", self.display_address)
+
+        self.address_label = CTkLabel(self, text = '', font = CTkFont(size = 12), compound='left')
+        self.address_label.grid(row = next(_frame_rows), column = 0, padx = 5, pady = 5)
+        
+    def display_address(self, event = None) -> None:
+        
+        self.address_label.configure(text = f'Address: {self.host_entry.get()}:{self.port_entry.get()}')
+    
+    
+        
 
 class MainFrame(CTkFrame):
 
