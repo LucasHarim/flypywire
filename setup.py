e05eb7a4c1a6780e7162f396b6fe01c6286adb4c
LucasHarim <harimlgc@usp.br>
2024-10-19 20:55:51 -0300

    Adding opencv as requirement. Updating description

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 
 setup(name='flypywire',
       version='1.0',
-      description='Exploring JSBSim and ZeroMQ functionalities',
+      description='Client-side of flypywire, an open-source and modular flight simulator for research.',
       author='Lucas Harim G. C.',
       author_email='harimlgc@usp.br',
       packages = find_packages(),
     install_requires = [
       'numpy==1.24.4',
+      'opencv-python',
       'jsbsim',
       'py-trees==0.8.3',
       'pyzmq',
